5e85f1adcc49810f13fa3a3ab4559b9f98078ad1
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-03-20 11:06:59 +0000

    Add a command-line interface for running the built-in web server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, SubCommand};
+
 mod web;
 
 fn main() {
-    web::run();
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .author(crate_authors!())
+        .subcommand(
+            SubCommand::with_name("web")
+                .about("The built-in web server")
+                .subcommand(SubCommand::with_name("run").about("Runs the built-in web server")),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("web") {
+        if let Some(_) = matches.subcommand_matches("run") {
+            web::run();
+        }
+    }
 }
