b21d386eed9af9182c012e5a6972249988726870
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-03-20 11:32:19 +0000

    Add a command-line option for configuring the web server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, SubCommand};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
 
 mod web;
 
@@ -10,13 +10,27 @@ fn main() {
         .subcommand(
             SubCommand::with_name("web")
                 .about("The built-in web server")
-                .subcommand(SubCommand::with_name("run").about("Runs the built-in web server")),
+                .subcommand(
+                    SubCommand::with_name("run")
+                        .about("Runs the built-in web server")
+                        .arg(
+                            Arg::with_name("config")
+                                .short("c")
+                                .long("config")
+                                .value_name("FILE")
+                                .help("The configuration file to use")
+                                .required(true),
+                        ),
+                ),
         )
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("web") {
-        if let Some(_) = matches.subcommand_matches("run") {
-            web::run();
+        if let Some(matches) = matches.subcommand_matches("run") {
+            match web::load_config(matches.value_of("config").unwrap()) {
+                Ok(config) => web::run(config),
+                Err(err) => println!("Error loading configuration: {}", err),
+            }
         }
     }
 }
--- src/web.rs
+++ src/web.rs
@@ -2,13 +2,30 @@ use actix::System;
 use actix_web::middleware::Logger;
 use actix_web::server::HttpServer;
 use actix_web::{App, HttpRequest};
+use serde::Deserialize;
+use std::error::Error;
+use std::net::Ipv4Addr;
+
+#[derive(Deserialize)]
+pub struct Config {
+    host: Ipv4Addr,
+    port: u16,
+    log_level: String,
+}
 
 fn index(_req: &HttpRequest) -> &'static str {
     "Hello world!"
 }
 
-pub fn run() {
-    std::env::set_var("RUST_LOG", "actix_web=info");
+pub fn load_config(path: &str) -> Result<Config, Box<dyn Error + 'static>> {
+    let string = std::fs::read_to_string(path)?;
+    let config = toml::from_str(&string)?;
+
+    Ok(config)
+}
+
+pub fn run(config: Config) {
+    std::env::set_var("RUST_LOG", format!("actix_web={}", config.log_level));
     env_logger::init();
 
     let system = System::new("brace");
@@ -18,10 +35,10 @@ pub fn run() {
             .middleware(Logger::default())
             .resource("/", |r| r.f(index))
     })
-    .bind("0.0.0.0:80")
+    .bind(format!("{}:{}", config.host, config.port))
     .unwrap()
     .start();
 
-    println!("Started http server: 0.0.0.0:80");
+    println!("Started http server: {}:{}", config.host, config.port);
     system.run();
 }
