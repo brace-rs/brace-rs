b9e8d47b696ddf91852c1529ab265ea7cd4012c0
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-03-23 13:51:31 +0000

    Add postgres database integration and connection pooling

---
+++ src/lib/util/db.rs
@@ -0,0 +1,37 @@
+use crate::config::db::DatabaseConfig;
+use actix::{Actor, Addr, SyncArbiter, SyncContext};
+use r2d2::Pool;
+use r2d2_postgres::{PostgresConnectionManager, TlsMode};
+use std::ops::Deref;
+
+pub struct Database(pub Addr<DatabaseInner>);
+
+impl Database {
+    pub fn new(conf: DatabaseConfig) -> Self {
+        let manager = PostgresConnectionManager::new(
+            format!(
+                "postgres://{}:{}@{}:{}/{}",
+                conf.username, conf.password, conf.host, conf.port, conf.database
+            ),
+            TlsMode::None,
+        )
+        .unwrap();
+        let pool = Pool::new(manager).unwrap();
+
+        Self(SyncArbiter::start(3, move || DatabaseInner(pool.clone())))
+    }
+}
+
+impl Deref for Database {
+    type Target = Addr<DatabaseInner>;
+
+    fn deref(&self) -> &Addr<DatabaseInner> {
+        &self.0
+    }
+}
+
+pub struct DatabaseInner(pub Pool<PostgresConnectionManager>);
+
+impl Actor for DatabaseInner {
+    type Context = SyncContext<Self>;
+}
