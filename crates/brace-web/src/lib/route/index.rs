db7565dd13cc6c750862e8bb71a30f5d5aa73779
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-04-02 00:23:30 +0100

    Update actix and actix-web to latest alpha releases

--- crates/brace-web/src/lib/route/index.rs
+++ crates/brace-web/src/lib/route/index.rs
@@ -1,12 +1,13 @@
-use actix_web::error::ErrorInternalServerError;
-use actix_web::{AsyncResponder, FutureResponse, HttpRequest, HttpResponse};
+use actix_web::error::{Error, ErrorInternalServerError};
+use actix_web::web::Data;
+use actix_web::HttpResponse;
 use brace_theme::renderer::Template;
 use futures::future::Future;
 use serde_json::json;
 
 use crate::state::AppState;
 
-pub fn get(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
+pub fn get(data: Data<AppState>) -> impl Future<Item = HttpResponse, Error = Error> {
     let template = Template::new(
         "index",
         json!({
@@ -15,13 +16,11 @@ pub fn get(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
         }),
     );
 
-    req.state()
-        .renderer()
+    data.renderer()
         .send(template)
         .map_err(ErrorInternalServerError)
         .and_then(|res| match res {
             Ok(body) => Ok(HttpResponse::Ok().content_type("text/html").body(body)),
             Err(err) => Err(ErrorInternalServerError(err)),
         })
-        .responder()
 }
