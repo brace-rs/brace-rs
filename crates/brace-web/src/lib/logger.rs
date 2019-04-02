5e09411591fde7f77c65b08b2a1f95c2c720798e
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-04-02 11:52:58 +0100

    Add support for multiple log backends (#22)

---
+++ crates/brace-web/src/lib/logger.rs
@@ -0,0 +1,43 @@
+use std::path::Path;
+
+use chrono::Local;
+use failure::Error;
+use fern::{log_file, Dispatch};
+use log::{warn, LevelFilter};
+use path_absolutize::Absolutize;
+
+use crate::config::{AppConfig, LogOutput};
+
+pub fn init(conf: &AppConfig, path: &Path) -> Result<(), Error> {
+    let logger = Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{} {} {}] {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%SZ"),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(LevelFilter::Off)
+        .level_for("brace", conf.web.log.level.clone().into())
+        .level_for("brace_web", conf.web.log.level.clone().into())
+        .level_for("actix_web", conf.web.log.level.clone().into());
+
+    match conf.web.log.output {
+        LogOutput::Stdout => logger.chain(std::io::stdout()).apply()?,
+        LogOutput::Stderr => logger.chain(std::io::stderr()).apply()?,
+        LogOutput::File => match &conf.web.log.file {
+            Some(file) => {
+                let path = path.join("..").join(file).absolutize()?;
+                logger.chain(log_file(path)?).apply()?;
+            }
+            None => {
+                logger.chain(std::io::stderr()).apply()?;
+                warn!("Invalid log file specified: {:?}", &conf.web.log.file);
+            }
+        },
+    }
+
+    Ok(())
+}
