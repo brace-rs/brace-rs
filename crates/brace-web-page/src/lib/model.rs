10102d25f7292dbf4889292c680ab1825da176cb
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-04-05 15:15:01 +0100

    Add datetime handling to web page forms

--- crates/brace-web-page/src/lib/model.rs
+++ crates/brace-web-page/src/lib/model.rs
@@ -8,7 +8,9 @@ pub struct Page {
     pub id: Uuid,
     pub title: String,
     pub content: String,
+    #[serde(with = "datetime")]
     pub created: DateTime<Utc>,
+    #[serde(with = "datetime")]
     pub updated: DateTime<Utc>,
 }
 
@@ -21,5 +23,30 @@ impl Default for Page {
             created: Utc::now(),
             updated: Utc::now(),
         }
+    }
+}
+
+mod datetime {
+    use chrono::{DateTime, NaiveDateTime, Utc};
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    pub fn serialize<S: Serializer>(
+        datetime: &DateTime<Utc>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
+        datetime
+            .format("%Y-%m-%dT%H:%M")
+            .to_string()
+            .serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(
+        deserializer: D,
+    ) -> Result<DateTime<Utc>, D::Error> {
+        let datetime: String = Deserialize::deserialize(deserializer)?;
+
+        Ok(NaiveDateTime::parse_from_str(&datetime, "%Y-%m-%dT%H:%M")
+            .map_err(serde::de::Error::custom)
+            .map(|datetime| DateTime::from_utc(datetime, Utc))?)
     }
 }
