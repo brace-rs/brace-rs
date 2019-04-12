8e550e29057b23b21c81f7bcdb59a0d6d87ad691
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-04-12 16:31:58 +0100

    Add JSON web token authentication for REST endpoints

--- crates/brace-web-auth/src/lib/route/api/create.rs
+++ crates/brace-web-auth/src/lib/route/api/create.rs
@@ -3,22 +3,33 @@ use actix_web::http::header;
 use actix_web::web::{Data, Json};
 use actix_web::HttpResponse;
 use brace_db::Database;
-use futures::future::Future;
+use futures::future::{ok, Either, Future};
 use serde_json::json;
 
-use crate::model::User;
+use crate::model::{CurrentAuth, User};
 
 pub fn create(
+    auth: CurrentAuth,
     database: Data<Database>,
     user: Json<User>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
-    crate::action::create::create(&database, user.into_inner())
-        .map_err(ErrorInternalServerError)
-        .and_then(|user| {
-            HttpResponse::Created()
-                .header(header::LOCATION, format!("/api/users/{}", user.id))
-                .json(json!({
-                    "value": user,
-                }))
-        })
+    match auth {
+        CurrentAuth::Unauthenticated => Either::A(ok(HttpResponse::Unauthorized()
+            .header(
+                "WWW-Authenticate",
+                r#"Bearer realm="localhost", charset="UTF-8""#,
+            )
+            .finish())),
+        CurrentAuth::Authenticated(_) => Either::B(
+            crate::action::create::create(&database, user.into_inner())
+                .map_err(ErrorInternalServerError)
+                .and_then(|user| {
+                    HttpResponse::Created()
+                        .header(header::LOCATION, format!("/api/users/{}", user.id))
+                        .json(json!({
+                            "value": user,
+                        }))
+                }),
+        ),
+    }
 }
--- crates/brace-web-page/src/lib/route/api/create.rs
+++ crates/brace-web-page/src/lib/route/api/create.rs
@@ -3,22 +3,34 @@ use actix_web::http::header;
 use actix_web::web::{Data, Json};
 use actix_web::HttpResponse;
 use brace_db::Database;
-use futures::future::Future;
+use brace_web_auth::model::CurrentAuth;
+use futures::future::{ok, Either, Future};
 use serde_json::json;
 
 use crate::model::Page;
 
 pub fn create(
+    auth: CurrentAuth,
     database: Data<Database>,
     page: Json<Page>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
-    crate::action::create::create(&database, page.into_inner())
-        .map_err(ErrorInternalServerError)
-        .and_then(|page| {
-            HttpResponse::Created()
-                .header(header::LOCATION, format!("/api/pages/{}", page.id))
-                .json(json!({
-                    "value": page,
-                }))
-        })
+    match auth {
+        CurrentAuth::Unauthenticated => Either::A(ok(HttpResponse::Unauthorized()
+            .header(
+                "WWW-Authenticate",
+                r#"Bearer realm="localhost", charset="UTF-8""#,
+            )
+            .finish())),
+        CurrentAuth::Authenticated(_) => Either::B(
+            crate::action::create::create(&database, page.into_inner())
+                .map_err(ErrorInternalServerError)
+                .and_then(|page| {
+                    HttpResponse::Created()
+                        .header(header::LOCATION, format!("/api/pages/{}", page.id))
+                        .json(json!({
+                            "value": page,
+                        }))
+                }),
+        ),
+    }
 }
